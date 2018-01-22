4cc260952744d0e8621605652b3311dcc8f270d0
Cédric Jimenez <happy.ceds@gmail.com>
2018-01-22 22:24:10 +0100

    Initial version with demo app and MingGW, GCC compilers

---
+++ src/libs/demo_lib/demo_lib.h
@@ -0,0 +1,26 @@
+/*
+Copyright(c) 2017 Cedric Jimenez
+
+This file is part of Nano-Build.
+
+Nano-Build is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Nano-Build is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with Nano-Build.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef DEMO_LIB_H
+#define DEMO_LIB_H
+
+const char* FILE1_It(void);
+const char* FILE2_Works(void);
+
+#endif /* DEMO_LIB_H */
